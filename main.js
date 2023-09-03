452430d1b79182343e2ffbe7f85eb92ee19c726c
skrill <143934804+skrill999@users.noreply.github.com>
2023-09-03 12:08:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,139 @@
+window.addEventListener("load", linkshit);
+function linkshit()
+{
+   const path = window.location.href.split("/")
+   const Text = path[path.length - 1].split('?')
+   if (Text[Text.length - 1] == "home")
+   {
+     window.location = "https://grannythedev.github.io";
+   }
+   else if (Text[Text.length - 1] == "android")
+   {
+     window.location = "https://grannythedev.github.io/androidmods.html";
+   }
+   else if (Text[Text.length - 1] == "ios")
+   {
+     window.location = "https://grannythedev.github.io/iOSApps.html";
+   }
+   else if (Text[Text.length - 1] == "save")
+   {
+     window.location = "https://grannythedev.github.io/SaveFiles.html";
+   }
+   else if (Text[Text.length - 1] == "bots")
+   {
+    window.location = "https://grannythedev.github.io/bots.html";
+   }
+   if (window.location.pathname == "/index.html")
+   {
+     let tab = document.getElementById("home");
+     if (tab != null)
+     {
+       tab.style.color = "#0000FF";
+     }
+   }
+   else if (window.location.pathname == "/androidmods.html")
+   {
+     let tab = document.getElementById("android");
+     if (tab != null)
+     {
+       tab.style.color = "#0000FF";
+     }
+   }
+   else if (window.location.pathname == "/iOSApps.html")
+   {
+     let tab = document.getElementById("ios");
+     if (tab != null)
+     {
+       tab.style.color = "#0000FF";
+     }
+   }
+   else if (window.location.pathname == "/SaveFiles.html")
+   {
+     let tab = document.getElementById("save");
+     if (tab != null)
+     {
+       tab.style.color = "#0000FF";
+     }
+   }
+   else if (window.location.pathname == "/bots.html")
+   {
+     let tab = document.getElementById("bots");
+     if (tab != null)
+     {
+       tab.style.color = "#0000FF";
+     }
+   }
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+let skrillbotbtn = document.getElementById("skrillbot");
+if (skrillbotbtn != null)
+{
+  skrillbotbtn.addEventListener("click", skrillbot);
+}
+let hotjennybtn = document.getElementById("hotjennybot");
+if (hotjennybtn != null)
+{
+  hotjennybtn.addEventListener("click", hotjenny);
+}
+let grannybtn = document.getElementById("granny");
+if (grannybtn != null)
+{
+  grannybtn.addEventListener("click", grannyclick);
+}
+let serverbtn = document.getElementById("serverfile");
+if (serverbtn != null)
+{
+  serverbtn.addEventListener("click", serverclick);
+}
+let gamebtn = document.getElementById("gamefile");
+if (gamebtn != null)
+{
+  gamebtn.addEventListener("click", gameclick);
+}
+let scripthunrepobtn = document.getElementById("scripthubrepo");
+if (scripthunrepobtn != null)
+{
+  scripthunrepobtn.addEventListener("click", scripthunrepoclick);
+}
+let cookieclickerbtn = document.getElementById("cookieclicker");
+if (cookieclickerbtn != null)
+{
+  cookieclickerbtn.addEventListener("click", cookieclickerclick);
+}
+});
+
+function skrillbot()
+{
+window.open('https://discord.com/api/oauth2/authorize?client_id=1057012274117746768&permissions=8&scope=bot');  
+}
+
+function hotjenny()
+{
+window.open('https://discord.com/api/oauth2/authorize?client_id=998895254734393344&permissions=8&scope=bot');  
+}
+
+function grannyclick()
+{
+  window.open('https://drive.google.com/file/d/1-GkHe0bTHhD3apS94tCeM2NXg1xOlAyJ/view?usp=drivesdk')
+}
+
+function serverclick()
+{
+  window.open('https://drive.google.com/file/d/1-6IeG_lKDlIpLYhSjHnJChnQm4Qa8Km2/view?usp=drivesdk')
+}
+
+function gameclick()
+{
+  window.open('https://drive.google.com/file/d/1-5UMRAxQC-RIyS0-0pu0DQjKYW1KZhee/view?usp=drivesdk')
+}
+
+function scripthunrepoclick()
+{
+  window.open('https://github.com/GrannyTheDev/skrilly')
+}
+
+function cookieclickerclick()
+{
+  window.open('https://grannythedev.github.io/CookieClicker')
+}
